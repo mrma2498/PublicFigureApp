4912d78b162f6772745f17d1e8e5583ebbb78c24
mrma2498 <mrodriguesaraujo@ipvc.pt>
2021-04-24 23:04:16 +0100

    funcionalidadeVerFilme

--- src/app/filme/filme.page.ts
+++ src/app/filme/filme.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-filme',
@@ -10,14 +11,78 @@ export class FilmePage implements OnInit {
 
   filme: any;
 
-  constructor(private rotaAtiva: ActivatedRoute, private router: Router) { }
+  idFilme: any;
+
+  
+
+  public idIMDB: any;
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      "content-type": "application/x-www-form-urlencoded",
+      "x-rapidapi-key": "bf678e5d9bmsh77a5fd8f2ed33d9p171431jsn8dfe8f67d090",
+      "x-rapidapi-host": "gowatch.p.rapidapi.com"
+    })
+  }
+
+  public infoStreaming: any;
+
+
+  constructor(private rotaAtiva: ActivatedRoute, private router: Router,public http: HttpClient) {
+
+
+   }
+
+
 
   ngOnInit() {
     this.rotaAtiva.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state){
         this.filme = this.router.getCurrentNavigation().extras.state.filme;
+
+        //Obtem id do filme selecionado
+        this.idFilme = this.filme.id;
+        this.getIMDB(this.idFilme);
+        
       }
     });
+
+    
   }
+
+  /**Obter a id do IMDB */
+ getIMDB(idFilme: string){
+  this.http.get(`https://api.themoviedb.org/3/movie/${idFilme}?api_key=2a79881ec32301f25df0803432b96e07&language=en-US`).subscribe(data => {
+    
+        this.idIMDB=data['imdb_id'];
+      
+  })
+}
+
+
+
+sendToStreaming(id: string) {
+
+
+  let body = {
+    "id": id,
+		"type": "movie",
+		"country": "us"
+  }
+
+  this.http.post("https://gowatch.p.rapidapi.com/lookup/title/imdb_id", body, this.httpOptions)
+    .subscribe(data => {
+      this.infoStreaming = data['offers'][0].url
+      let url = this.infoStreaming
+      window.open(url);
+     }, error => {
+      console.log(error);
+      //Mostrar erro "Indisponível"
+    });
+
+
+  
+}
+
 
 }
--- src/app/films/films.page.ts
+++ src/app/films/films.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders,HttpErrorResponse } from '@angular/common/http';
 import {NavigationExtras, Router} from '@angular/router';
-import { StringMap } from '@angular/compiler/src/compiler_facade_interface';
 
 
 
@@ -16,18 +15,30 @@ export class FilmsPage implements OnInit {
 
 public filmes: any;
 
+
+public idIMDB: any;
+
+
+
+
+
+
+
   /**Faz uma chamada GET à API, para obter todos os filmes do ator com id 131, e insere o resultado da chamada na váriavel "filmes"*/
 constructor (public http: HttpClient, private router: Router) {
     this.http.get('https://api.themoviedb.org/3/person/131/movie_credits?api_key=2a79881ec32301f25df0803432b96e07&language=pt-PT').subscribe(data => {
-      this.filmes=data['cast'];
-      console.log(this.filmes);
+  
+      
+        this.filmes=data['cast'];
+        
+      //console.log(this.filmes);
   })
+
   }
 
 
-
   ngOnInit() {
-  
+    
         
   }
 
@@ -35,15 +46,21 @@ constructor (public http: HttpClient, private router: Router) {
   verDetalhesFilme(id: string){
     let extras: NavigationExtras;
 
+
     extras = {
       state: {
       filme: this.filmes[id] 
+      
     }
   }
 
   this.router.navigate(['/filme'], extras);
+ 
+ 
 }
 
-  
+
+
+
 
 }
